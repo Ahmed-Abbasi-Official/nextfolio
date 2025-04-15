12564e569de3cb24fbb757a03b5a9937ce5c4ace
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-04-15 17:19:25 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Hero from '@/components/sections/hero';
 import About from '@/components/sections/about';
-import Experience from '@/components/sections/experience';
 import Projects from '@/components/sections/projects';
 import Services from '@/components/sections/services';
 import Contact from '@/components/sections/contact';
